e1367b8a43942c6ceb2deee6bd2c2a957079963e
francis omondi <francisomondi17@gmail.com>
2022-07-28 19:33:55 +0300

    the global variable, set interval

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-console.log("this is my test page")
+const reserved = (name) => {
+    console.log(`Hllo ${name}, your reservation has been received`)
+}
+reserved("Francis")
+reserved("Edna")
+
+global.setTimeout(() => {
+    console.log("this will show after 3 seconds of running this code")
+}, 3000);
