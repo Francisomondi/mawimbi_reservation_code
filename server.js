a9c97c58bb721c6b8816689e2ef0d5b09ce67bba
francis omondi <francisomondi17@gmail.com>
2022-08-03 01:02:46 +0300

    setting headers, displaying items to the browser via the res variable

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log("request made")
+    console.log(req.url, req.method)
+
+    res.setHeader("content-type", "text/html")
+    res.write("<h1>hello Francis</h1>")
+    res.write("<p>this is where all the frontend html is displayed</p>")
 })
-server.listen(3000, () => {
+server.listen(3000, "localhost", () => {
     console.log("server started on port 3000")
 })
