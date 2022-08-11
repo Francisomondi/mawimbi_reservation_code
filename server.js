4a65df5c0420520ce30c45778aa8c82d2523bea5
francis omondi <francisomondi17@gmail.com>
2022-08-11 17:47:11 +0300

    senfing html files

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
     //setting meader
-    res.setHeader("Content-type", "text/html");
+    fs.readFile("./views/index.html", (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
 
-    res.write("<head> <link rel='stylesheet' href='style.css'></head>");
-    res.write("<h2> Mawimbi Sea Food Restaurant </h2>");
-    res.write("<p>this is going to be a mawimbi sea food restaurant reservation form </p>");
-    res.end();
+            res.end(data);
+        }
+    })
 
 
 });
