c3ab9057dc098b88d17a7589b04d83f185370b55
francis omondi <francisomondi17@gmail.com>
2022-08-11 19:09:45 +0300

    returning dynamic html pages

--- server.js
+++ server.js
@@ -4,14 +4,30 @@ const fs = require("fs");
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
-    //setting meader
-    fs.readFile("./views/index.html", (err, data) => {
+    //setting header content type
+    res.setHeader("Content-type", "text/html")
+
+    let path = "./views/";
+
+    switch (req.url) {
+        case "/":
+            path += "index.html";
+            break;
+        case "/about":
+            path += "about.html";
+            break;
+        default:
+            path += "404.html";
+            break;
+    }
+
+    fs.readFile(path, (err, data) => {
         if (err) {
             console.log(err);
             res.end();
         } else {
-
-            res.end(data);
+            res.write(data);
+            res.end();
         }
     })
 
