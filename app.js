b867db5e66a98f75b99366deb53b600831413393
francis omondi <francisomondi17@gmail.com>
2022-08-12 15:10:58 +0300

    reservation form

--- app.js
+++ app.js
@@ -3,30 +3,25 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.listen(3000);
 
 app.get("/", (req, res) => {
     //res.send("<h1>Home Page</h1> ");
-    res.sendFile("./views/index.html", {
-        root: __dirname
-    });
+    res.render("index");
 });
 
 app.get("/about", (req, res) => {
     //res.send("<h1>About Page</h1> ");
-    res.sendFile("./views/about.html", {
-        root: __dirname
-    });
+    res.render("about")
 });
 
-//redirect
-app.get("/about-us", (req, res) => {
-    res.redirect("/about");
-});
-
+//create reservation
+app.get("/reservation/create", (req, res) => {
+    res.render("create")
+})
 //404 page
 app.use((req, res) => {
-    res.sendFile("./views/404.html", {
-        root: __dirname
-    });
+    res.status(404).render("404");
 });
