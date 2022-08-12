3efd94e033912db42c5e4245c3e373689dd4de7a
francis omondi <francisomondi17@gmail.com>
2022-08-12 18:47:42 +0300

    disp[laying data to views using ejs

--- app.js
+++ app.js
@@ -6,22 +6,61 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.listen(3000);
-
+//home route
 app.get("/", (req, res) => {
-    //res.send("<h1>Home Page</h1> ");
-    res.render("index");
-});
+    const reservations = [{
+            firstname: "Francis",
+            email: "Francisomondi17@gmail.com",
+            time: "!0:45",
+            phone: "0740694770",
+            request: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempors"
+        },
+        {
+            firstname: "Oscar",
+            email: "OsooOscar@gmail.com",
+            time: "10:45",
+            phone: "0740694770",
+            request: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempors"
+        },
+        {
+            firstname: "Edna Odongo",
+            email: "Ednaodongothe1st@gmail.com",
+            time: "11:45",
+            phone: "0700000111",
+            request: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempors"
+        },
+        {
+            firstname: "Fredrick Ochieng",
+            email: "Fredochiepapa@gmail.com",
+            time: "13:45",
+            phone: "0759347325",
+            request: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempors"
+        },
+    ];
 
+    res.render("index", {
+        title: "Home",
+        reservations
+    });
+});
+//about route
 app.get("/about", (req, res) => {
-    //res.send("<h1>About Page</h1> ");
-    res.render("about")
+
+    res.render("about", {
+        title: "About",
+
+    });
 });
 
 //create reservation
 app.get("/reservation/create", (req, res) => {
-    res.render("create")
+    res.render("create", {
+        title: "create reservation"
+    })
 })
 //404 page
 app.use((req, res) => {
-    res.status(404).render("404");
+    res.status(404).render("404", {
+        title: "404"
+    });
 });
