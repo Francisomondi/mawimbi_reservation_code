3f6ea310b7e007867302de2115015c7befc4c9b5
francis omondi <francisomondi17@gmail.com>
2022-08-18 18:39:07 +0300

    outputing all our reservations to view

--- app.js
+++ app.js
@@ -25,86 +25,10 @@ app.use(express.static("public"));
 
 //mongoose and  mongo sandbox routes
 //saving reservations to mondodb
-app.get("/add-reservation", (req, res) => {
-    const reservation = new Reservation({
-        firstname: "jeremy",
-        lastname: "Oguda",
-        email: "geremyoguda@gmail.com",
-        guests: "7",
-        date: "09/12/2022",
-        time: "11:45",
-        country: "Kenya",
-        Phone: "0740694770",
-        request: "parte after parte"
-    });
-    reservation.save()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err)
-        });
-});
-//fetching all reservations
-app.get("/all-reservations", (req, res) => {
-    Reservation.find()
-        .then((result) => {
-            res.send(result)
-
-        })
-        .catch((err) => console.log(err));
-});
-
-//finding single reservation
-
-app.get("/single-reservation", (req, res) => {
-    Reservation.findById("62fcf26a9d0908cfb2746cc8")
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
-
-
 
 //home route
 app.get("/", (req, res) => {
-    const reservations = [{
-            firstname: "Francis",
-            email: "Francisomondi17@gmail.com",
-            time: "!0:45",
-            phone: "0740694770",
-            request: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempors"
-        },
-        {
-            firstname: "Oscar",
-            email: "OsooOscar@gmail.com",
-            time: "10:45",
-            phone: "0740694770",
-            request: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempors"
-        },
-        {
-            firstname: "Edna Odongo",
-            email: "Ednaodongothe1st@gmail.com",
-            time: "11:45",
-            phone: "0700000111",
-            request: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempors"
-        },
-        {
-            firstname: "Fredrick Ochieng",
-            email: "Fredochiepapa@gmail.com",
-            time: "13:45",
-            phone: "0759347325",
-            request: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempors"
-        },
-    ];
-
-    res.render("index", {
-        title: "Home",
-        reservations: reservations
-    });
+    res.redirect("/reservations")
 });
 //about route
 app.get("/about", (req, res) => {
@@ -115,7 +39,22 @@ app.get("/about", (req, res) => {
     });
 });
 
-//create reservation
+// reservation routes
+app.get("/reservations", (req, res) => {
+    Reservation.find()
+        .then((result) => {
+            res.render("index", {
+                title: "All Reservations",
+                reservations: result
+            })
+
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+
 app.get("/create", (req, res) => {
     res.render("create", {
         title: "create reservation"
